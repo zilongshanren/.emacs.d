0b27710d460950d99bb6988a0fa141ba9269134d
zilongshanren <guanghui8827@126.com>
2016-04-08 00:32:20 +0800

    fix keybindings

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -15,4 +15,9 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
+(global-set-key (kbd "C-c p f") 'counsel-git)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+
 (provide 'init-keybindings)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,6 +4,5 @@
 (setq org-src-fontify-natively t)
 
 (setq org-agenda-files '("~/org"))
-(global-set-key (kbd "C-c a") 'org-agenda)
 
 (provide 'init-org)
