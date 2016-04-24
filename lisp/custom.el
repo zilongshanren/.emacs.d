71c5c2bf51119f4589073f96f70b9ab263e282ce
zilongshanren <guanghui8827@126.com>
2016-04-24 12:23:50 +0800

    add packages

--- lisp/custom.el
+++ lisp/custom.el
@@ -7,10 +7,13 @@
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
    (quote
-    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default))))
+    ("6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" default)))
+ '(popwin:popup-window-position (quote right))
+ '(popwin:popup-window-width 90))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(iedit-occurrence ((t (:inherit region))))
  '(js2-external-variable ((t (:foreground "dark gray")))))
--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -24,6 +24,12 @@
 (recentf-mode 1)			
 (setq recentf-max-menu-items 25)
 
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight enclosing parens."
+  (cond ((looking-at-p "\\s(") (funcall fn))
+        (t (save-excursion
+             (ignore-errors (backward-up-list))
+             (funcall fn)))))
 
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
@@ -68,4 +74,32 @@
 
 (setq dired-dwim-target t)
 
+(defun hidden-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
+(defun remove-dos-eol ()
+  "Replace DOS eolns CR LF with Unix eolns CR"
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t) (replace-match "")))
+
+
+;; dwin = do what i mean.
+  (defun occur-dwim ()
+    "Call `occur' with a sane default."
+    (interactive)
+    (push (if (region-active-p)
+              (buffer-substring-no-properties
+               (region-beginning)
+               (region-end))
+            (let ((sym (thing-at-point 'symbol)))
+              (when (stringp sym)
+                (regexp-quote sym))))
+          regexp-history)
+    (call-interactively 'occur))
+
+  (global-set-key (kbd "M-s o") 'occur-dwim)
 (provide 'init-better-defaults)
