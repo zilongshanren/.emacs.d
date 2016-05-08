e8f4877712e3ba92b43e72897cc53d8c7f479cd9
zilongshanren <guanghui8827@126.com>
2016-05-08 21:13:37 +0800

    add meo-theme

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Added by Package.el.  This must come before configurations of
+; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
@@ -23,7 +23,9 @@
 
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
 
+(require 'moe-theme)
 (moe-dark)
+
 
 (load-file custom-file)
 
