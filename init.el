07bafeaa1ff61c556ec027a44cfc88b981a79731
lionqu <lionqu@tencent.com>
2022-01-12 08:30:40 +0800

    fix error on mac

--- init.el
+++ init.el
@@ -75,17 +75,13 @@
 
  (setq package-selected-packages my/packages)
 
- (defun my/packages-installed-p ()
-     (cl-loop for pkg in my/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
 
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
+	 
+     
+	 
      (dolist (pkg my/packages)
        (when (not (package-installed-p pkg))
-	 (package-install pkg))))
+	 (package-install pkg)))
 	 
 (use-package benchmark-init
   :ensure t
