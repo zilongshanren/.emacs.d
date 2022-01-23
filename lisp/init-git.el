132b125a751a7e8ea7996706a9d08eaa5229018a
zilongshanren <guanghui8827@gmail.com>
2022-01-23 22:51:23 +0800

    fix magit keybindings

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -26,14 +26,15 @@
 
 (use-package magit
   :config
-  (evil-add-hjkl-bindings magit-status-mode-map
-    'emacs
-    (kbd "l") 'nil
-    (kbd "h") 'nil
-    (kbd "C-u") 'evil-scroll-up
-    (kbd "C-d") 'evil-scroll-down
-    (kbd "K") 'magit-discard
-    (kbd "s-1") 'magit-jump-to-unstaged
-    (kbd "s-2") 'magit-jump-to-untracked))
+  (with-eval-after-load 'evil
+    (evil-add-hjkl-bindings magit-status-mode-map
+      'emacs
+      (kbd "l") 'nil
+      (kbd "h") 'nil
+      (kbd "C-u") 'evil-scroll-up
+      (kbd "C-d") 'evil-scroll-down
+      (kbd "K") 'magit-discard
+      (kbd "s-1") 'magit-jump-to-unstaged
+      (kbd "s-2") 'magit-jump-to-untracked)))
 
 (provide 'init-git)
