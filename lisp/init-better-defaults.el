373d9aba668879cbd8a372c235bc9f5a4545d154
zilongshanren <guanghui8827@gmail.com>
2022-01-23 23:35:14 +0800

    add better defaults

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -39,7 +39,11 @@
   (smartparens-global-mode t)
   (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
   (sp-local-pair 'emacs-lisp-mode "`" nil :actions nil)
-  (sp-local-pair 'lisp-interaction-mode "'" nil :actions nil))
+  (sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
+  :config
+    (sp-with-modes
+        '(c++-mode objc-mode c-mode)
+      (sp-local-pair "{" nil :post-handlers '(:add ("||\n[i]" "RET")))))
 
 (use-package hungry-delete
   :init
