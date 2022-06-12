ebd3735f09c820d88d269e1fb381e98043c0db8f
zilongshanren <guanghui8827@gmail.com>
2022-06-12 11:13:20 +0800

    fix filename

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -118,7 +118,7 @@
 ;; (set-default-coding-systems 'utf-8)
 ;; (set-buffer-file-coding-system 'utf-8)
 ;; (set-clipboard-coding-system 'utf-8)
-;; (set-file-name-coding-system 'utf-8)
+(set-file-name-coding-system 'gbk)
 ;; (set-keyboard-coding-system 'utf-8)
 ;; (set-terminal-coding-system 'utf-8)
 ;; (set-selection-coding-system 'utf-8)
