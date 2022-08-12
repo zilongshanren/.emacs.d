11ea1ceb58cd109b93ddffab893ba1ed107487e0
zilongshanren <guanghui8827@126.com>
2022-08-12 22:04:19 +0800

    add python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -24,6 +24,20 @@
 ;; Floor, Boston, MA 02110-1301, USA.
 ;;
 
+;; Python Mode
+;; Install: pip install pyflakes autopep8
+(use-package python
+  :ensure t
+  :hook (inferior-python-mode . (lambda ()
+                                  (process-query-on-exit-flag
+                                   (get-process "Python"))))
+  :init
+  ;; Disable readline based native completion
+  (setq python-shell-completion-native-enable nil)
+  :config
+  (setq python-shell-interpreter "python3")
 
+  ;; Live Coding in Python
+  (use-package live-py-mode))
 
 (provide 'init-python)
