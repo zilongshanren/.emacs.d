e9a88dd617fffc95488aa95756cc2b4b222c9377
zilongshanren <guanghui8827@126.com>
2022-11-21 15:01:17 +0800

    tweak rust keybindings

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -27,6 +27,7 @@
 (use-package rust-mode
   :hook (rust-mode . my/rust-compile)
   :config
+  (define-key rust-mode-map (kbd "C-c C-c") nil)
   (setq rust-format-on-save nil)
   (global-leader
     :major-modes
@@ -34,7 +35,10 @@
     ;;and the keymaps:
     :keymaps
     '(rust-mode-map)
-    "=" 'rust-format-buffer)
+    "=" 'rust-format-buffer
+    "c" 'rust-compile
+    "r" 'rust-run
+    "t" 'rust-test)
   (define-key rust-mode-map (kbd "RET") 'av/auto-indent-method-maybe)
   (defun my/rust-compile ()
     (setq-local compile-command "cargo check --color never --tests")))
@@ -43,22 +47,28 @@
 (use-package cargo
   :hook ((rust-mode . cargo-minor-mode))
   :config
+  (setq shell-command-switch "-ic")
   (defun my/cargo-test-current ()
     (interactive)
     (setenv "RUST_LOG" "debug")
-    (setq shell-command-switch "-ic")
     (cargo-process-current-test))
+  (define-key cargo-mode-map (kbd "C-c C-c") nil)
   :bind (:map rust-mode-map
-         (("C-c C-t" . my/cargo-test-current)))
+              (("C-c C-t" . my/cargo-test-current)))
   :custom ((cargo-process--command-current-test "test --color never")
            (cargo-process--enable-rust-backtrace t)))
 
 (use-package rust-playground
-  :defer t
+  :hook ((rust-mode . rust-playground-mode))
   :custom (rust-playground-run-command "cargo run --color never")
   :commands (rust-playground-get-snippet-basedir)
   :config
+  (add-hook 'conf-toml-mode 'rust-playground-mode)
   (setq rust-playground-basedir (expand-file-name "~/workspace/rust/playground")))
+
+(use-package conf-toml-mode
+  :ensure nil
+  :hook ((conf-toml-mode . rust-playground-mode)))
 
 
 (provide 'init-rust)
