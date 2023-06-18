6e9724831efdc5eb0ce9754ab8728bd4fbb3649a
zilongshanren <guanghui8827@126.com>
2023-06-18 19:52:24 +0800

    tweak tab-bar mode

--- lisp/init-persp.el
+++ lisp/init-persp.el
@@ -24,6 +24,50 @@
 ;; Floor, Boston, MA 02110-1301, USA.
 ;;
 
+(use-package tab-bar-mode
+  :ensure nil
+  :init
+  (tab-bar-mode t)
+  (setq tab-bar-new-tab-choice "*scratch*") ;; buffer to show in new tabs
+  (setq tab-bar-close-button-show nil)      ;; hide tab close / X button
+  (setq tab-bar-show 1)                     ;; hide bar if <= 1 tabs open
+  (setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
+
+  (custom-set-faces
+   '(tab-bar ((t (:inherit mode-line))))
+   '(tab-bar-tab ((t (:inherit mode-line :foreground "353644"))))
+   '(tab-bar-tab-inactive ((t (:inherit mode-line-inactive :foreground "black")))))
+
+  (defvar ct/circle-numbers-alist
+    '((0 . "⓪")
+      (1 . "①")
+      (2 . "②")
+      (3 . "③")
+      (4 . "④")
+      (5 . "⑤")
+      (6 . "⑥")
+      (7 . "⑦")
+      (8 . "⑧")
+      (9 . "⑨"))
+    "Alist of integers to strings of circled unicode numbers.")
+
+  (defun ct/tab-bar-tab-name-format-default (tab i)
+    (let ((current-p (eq (car tab) 'current-tab))
+          (tab-num (if (and tab-bar-tab-hints (< i 10))
+                       (alist-get i ct/circle-numbers-alist) "")))
+      (propertize
+       (concat tab-num
+               " "
+               (alist-get 'name tab)
+               (or (and tab-bar-close-button-show
+                        (not (eq tab-bar-close-button-show
+                                 (if current-p 'non-selected 'selected)))
+                        tab-bar-close-button)
+                   "")
+               " ")
+       'face (funcall tab-bar-tab-face-function tab))))
+  (setq tab-bar-tab-name-format-function #'ct/tab-bar-tab-name-format-default)
+  (setq tab-bar-tab-hints t))
 
 
 (use-package tabspaces
