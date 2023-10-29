a51c894bf399910a4496aab3cf9445010e34b6b1
zilongshanren <guanghui8827@126.com>
2023-10-30 00:01:17 +0800

    add ci status

--- test-startup.sh
+++ test-startup.sh
@@ -7,6 +7,7 @@ ${EMACS:=emacs} -nw --batch \
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
+                              (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook))))'
 echo "Startup successful"
