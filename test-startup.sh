138dcecaf9c0cddd347b059cd27ade99117e45ca
zilongshanren <guanghui8827@126.com>
2023-10-30 09:09:51 +0800

    make ci happy

--- test-startup.sh
+++ test-startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 echo "Attempting startup..."
+export HOME="/home/runner/work/.emacs.d"; \
 emacs -q --batch \
         --eval "(message \"Testing...\")" \
         --eval "(let ((early-init-file (locate-user-emacs-file \"early-init.el\"))
