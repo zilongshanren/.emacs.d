f6d537a08142e7f7642c9b02f3aecdd1a4226821
zilongshanren <guanghui8827@126.com>
2023-11-01 20:40:26 +0800

    try to fix load path

--- test-startup.sh
+++ test-startup.sh
@@ -7,8 +7,7 @@ ${EMACS:=emacs} -nw --batch \
                         (let ((debug-on-error t)
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
-                              (user-init-file (expand-file-name "init.el"))
-                              (load-path (delq default-directory load-path)))
+                              (user-init-file (expand-file-name "init.el")))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook))))'
 echo "Startup successful"
