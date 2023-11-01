1fa90a66c3af112a91ec2e26a383a07e85cca306
zilongshanren <guanghui8827@126.com>
2023-11-01 20:08:38 +0800

    try to make ci happy

--- init.el
+++ init.el
@@ -68,7 +68,7 @@
 ;; Optimize: Force "lisp"" and "site-lisp" at the head to reduce the startup time.
 (defun update-load-path (&rest _)
   "Update `load-path'."
-  (dolist (dir '("site-lisp" "lisp"))
+  (dolist (dir '("site-lisp" "lisp" "elpa"))
     (push (expand-file-name dir user-emacs-directory) load-path)))
 
 (defun add-subdirs-to-load-path (&rest _)
